a609f2dc35cd3e37ab9e076d264f454355827909
saama <hjpw237@naver.com>
2020-01-13 09:52:00 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,5 +1,5 @@
 public class test{
-  rivate List<KeyVersion> processEventsInBucket(String bucketName) {
+  private List<KeyVersion> processEventsInBucket(String bucketName) {
       final AmazonS3 s3Client = EventHandler.getS3Client();
       ObjectListing files = s3Client.listObjects(bucketName);
   }
