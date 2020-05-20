060f8fc5fc61a755cea48e065c5d6f4ecab921bd
saama <hjpw237@naver.com>
2020-05-20 18:11:14 +0900

    Update and rename Exam29_ to Exam29_Programers_level2_4.java

--- programmers/Exam29_Programers_level2_4.java
+++ programmers/Exam29_Programers_level2_4.java
@@ -1,4 +1,8 @@
 class Solution {
+	//문제의 이해
+	//입력 [1, 2, 3, 2, 3]	
+	//출력 [4, 3, 1, 1, 0]
+	//각 배열의 인덱스 시점부터 종료시까지 주식가격이 떨어졌는지만 판단하면 됨
         public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
         for (int i = 0; i < answer.length; i++) {
