da69fcea7dcc7c765b04f962fb914cc7b1362212
saama <hjpw237@naver.com>
2020-06-18 22:13:15 +0900

    Update Solution1.java

--- cos_level1/Exam01/Solution1.java
+++ cos_level1/Exam01/Solution1.java
@@ -5,9 +5,14 @@ import java.util.Stack;
 
 class Solution1 {
     public long solution(long num) {
+        /*9진수 숫자를 매개변수로 받아 +1 시킨수를 9진수로 반환하는 문제
+        *9진수에 +1 한뒤 (+1함과 동시에 0이 포함될수있음)
+        *그 숫자에 digit(1,10,100,1000...)을 나눠서 0이 되는놈(위에 말한 +1이됐을때 0이 포함되는애들)
+        *에 현재 digit을 더해주어 0을 없앤다.
+        */
         num++;
         long digit = 1;
-        while (num / digit % 10 == 0) {
+        while (num / digit % 10 == 0) { //num에서 digit나눈거에 10의 나머지를 확인해야함!!
             System.out.println(num+" "+digit);
             num += digit;
             digit *= 10;
