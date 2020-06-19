eaa49d6feabe7bf67b0998e017b23da3c8369963
saama <hjpw237@naver.com>
2020-06-19 13:11:07 +0900

    Create Solution9.java

---
+++ cos_level1/Exam01/Solution9.java
@@ -0,0 +1,34 @@
+package algorithm;
+
+class Solution9 {
+    public String func_a(String str, int len){
+        String padZero = "";
+        int padSize = len-str.length(); //앞에서부터 0을 채움
+        for(int i = 0; i < padSize; i++)
+            padZero += "0";
+        return padZero + str;
+    }
+    
+    public int solution(String binaryA, String binaryB) {
+        int maxLength = Math.max(binaryA.length(), binaryB.length());
+        binaryA = func_a(binaryA, maxLength);
+        binaryB = func_a(binaryB, maxLength);
+        
+        int hammingDistance = 0;
+        for(int i = 0; i < maxLength; i++)
+            if(binaryA.charAt(i)!=binaryB.charAt(i)) //같은위치에 값이 다른문자의 갯수를 셈
+                hammingDistance += 1;
+        return hammingDistance;
+    }
+
+    // The following is main method to output testcase.
+    public static void main(String[] args) {
+    	Solution9 sol = new Solution9();
+        String binaryA = "10010";
+        String binaryB = "110";
+        int ret = sol.solution(binaryA, binaryB);
+        
+        // Press Run button to receive output. 
+        System.out.println("Solution: return value of the method is " + ret + " .");
+    }
+}
